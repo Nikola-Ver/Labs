d66f6f45b1f7dd8b7e776a495a4899c346a35121
Nikola-Ver <nikolveresh@gmail.com>
2021-05-05 13:05:23 +0300

    added kotlin

---
+++ mobile/Kotlin/app/src/main/java/com/example/schedule/ui/map/DashboardFragment.kt
@@ -0,0 +1,25 @@
+package com.example.schedule.ui.map
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.schedule.R
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+                ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_map, container, false)
+        return root
+    }
+}
---
+++ mobile/Kotlin/app/src/main/java/com/example/schedule/ui/schedule/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.schedule.ui.schedule
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.schedule.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProvider(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_schedule, container, false)
+        return root
+    }
+}
