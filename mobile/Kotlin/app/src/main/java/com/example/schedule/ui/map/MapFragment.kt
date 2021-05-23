ca1fcb43cc002e5ba8ed5cdefd6dc97963808bcc
Nikola-Ver <nikolveresh@gmail.com>
2021-05-23 16:21:09 +0300

    google map is ready

--- mobile/Kotlin/app/src/main/java/com/example/schedule/ui/map/MapFragment.kt
+++ mobile/Kotlin/app/src/main/java/com/example/schedule/ui/map/MapFragment.kt
@@ -13,34 +13,26 @@ import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : Fragment(), OnMapReadyCallback {
+class MapFragment : Fragment() {
     private lateinit var mView: View
     private lateinit var mMapView: MapView
     private lateinit var mMap: GoogleMap
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        mView = inflater.inflate(R.layout.fragment_map, container, false)
-        return mView
+    private val callback = OnMapReadyCallback { googleMap ->
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        mMapView = mView.findViewById(R.id.map)
-//        if (mMapView != null) {
-//            mMapView.onCreate(null)
-//            mMapView.onResume()
-//            mMapView.getMapAsync(this)
-//        }
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-//        mMap = googleMap
-//        val sydney = LatLng(-34.0, 151.0)
-//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
 }
